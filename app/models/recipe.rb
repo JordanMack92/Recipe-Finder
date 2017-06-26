051c83b8882c12406b6ebbf566a7ee110f9bffd6
Jordan Mack <jmack1248@gmail.com>
2017-06-26 18:28:42 -0400

    fix recipe.base_uri

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe
 	key_value = ENV["FOOD2FORK_KEY"] = '4d1927cd8059225337289f41b7435e68'
 	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 	
-	base_uri 'http://www.food2fork.com/api'
+	base_uri "http://#{hostport}/api"
 
 	default_params key: key_value
   	format :json
